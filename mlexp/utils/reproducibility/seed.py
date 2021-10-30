69d2058f77e4f08390201fa6dfd05f236bdb25b8
OtsuKotsu <otoxukk.y@gmail.com>
2021-10-30 10:42:52 +0900

    add docstring

--- mlexp/utils/reproducibility/seed.py
+++ mlexp/utils/reproducibility/seed.py
@@ -7,6 +7,11 @@ SEED = 3407
 
 
 def fix_random_seed(specified_seed: int = SEED):
+    """fix random seed of torch, numpy, python
+
+    Args:
+        specified_seed (int, optional): random seed. Defaults to SEED=3407.
+    """
     random.seed(specified_seed)
     numpy.random.seed(seed=specified_seed)
     torch.manual_seed(seed=specified_seed)
