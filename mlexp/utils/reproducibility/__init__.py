2e7611c5025d511c4379876ccfd52f7966a7de3f
OtsuKotsu <otoxukk.y@gmail.com>
2021-11-17 20:52:51 +0900

    add `ReproducibleDataLoader`

--- mlexp/utils/reproducibility/__init__.py
+++ mlexp/utils/reproducibility/__init__.py
@@ -1,10 +1,12 @@
 """This module includes some functions to keep reproducibility"""
+from mlexp.utils.data.dataloader import ReproducibleDataLoader
 from mlexp.utils.reproducibility.restrict_torch import behave_deterministically
 from mlexp.utils.reproducibility.seed import fix_random_seed
 
 __all__ = [
     "behave_deterministically",
     "fix_random_seed",
+    "ReproducibleDataLoader",
 ]
 
 # Please keep this list sorted
